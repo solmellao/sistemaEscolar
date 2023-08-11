988c5e8054dc4c17dec37de1cfb9865f5fb3a656
solmellao <114704343+solmellao@users.noreply.github.com>
2023-08-11 20:04:56 -0300

    BaseDeDatosActualizada

--- sistema_de_gestion_escolar.sql
+++ sistema_de_gestion_escolar.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 08-07-2023 a las 01:14:46
+-- Tiempo de generación: 12-08-2023 a las 01:01:43
 -- Versión del servidor: 10.4.28-MariaDB
 -- Versión de PHP: 8.2.4
 
@@ -28,7 +28,7 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `cursos` (
-  `Id` int(11) NOT NULL,
+  `id` int(11) NOT NULL,
   `nombre` varchar(50) DEFAULT NULL,
   `descripcion` varchar(100) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
@@ -37,13 +37,14 @@ CREATE TABLE `cursos` (
 -- Volcado de datos para la tabla `cursos`
 --
 
-INSERT INTO `cursos` (`Id`, `nombre`, `descripcion`) VALUES
+INSERT INTO `cursos` (`id`, `nombre`, `descripcion`) VALUES
 (1, 'civica', 'constitucion,articulos,preambulo nacional'),
 (2, 'LAS', 'literatura,arte y sociedad'),
 (3, 'COE', 'comunicacion oral y escrita'),
 (4, 'desarrollo de aplicaciones', 'programar app,diseño UXUI'),
 (5, 'matematica', 'algebra, circuitos logicos,combinatoria'),
-(6, 'ingles', 'presente simple,presente continuo,adverbios,articulos,');
+(6, 'ingles', 'presente simple,presente continuo,adverbios,articulos,'),
+(8, 'Curso prueba', 'testing');
 
 -- --------------------------------------------------------
 
@@ -52,7 +53,7 @@ INSERT INTO `cursos` (`Id`, `nombre`, `descripcion`) VALUES
 --
 
 CREATE TABLE `estudiantes` (
-  `Id` int(11) NOT NULL,
+  `id` int(11) NOT NULL,
   `nombre` varchar(50) DEFAULT NULL,
   `edad` int(11) DEFAULT NULL,
   `grado` varchar(50) DEFAULT NULL
@@ -62,13 +63,37 @@ CREATE TABLE `estudiantes` (
 -- Volcado de datos para la tabla `estudiantes`
 --
 
-INSERT INTO `estudiantes` (`Id`, `nombre`, `edad`, `grado`) VALUES
+INSERT INTO `estudiantes` (`id`, `nombre`, `edad`, `grado`) VALUES
 (1, 'Ana Sandoval', 13, '1er año'),
 (2, 'Analia Mora', 17, '4to año'),
 (3, 'Gonzalo Regueiro', 13, '1er año'),
 (4, 'Caterine Pardo', 19, '6to año'),
 (5, 'Gian Perilli', 16, '3er año'),
-(10, 'Ana Sandoval', 13, '1er año');
+(10, 'Ana Sandoval', 13, '1er año'),
+(11, 'Pepe', 12, '1er anio'),
+(12, 'Pepe', 12, '1er anio');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `estudiantes_cursos`
+--
+
+CREATE TABLE `estudiantes_cursos` (
+  `id_curso` int(11) NOT NULL,
+  `id_estudiante` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `estudiantes_cursos`
+--
+
+INSERT INTO `estudiantes_cursos` (`id_curso`, `id_estudiante`) VALUES
+(1, 2),
+(3, 4),
+(4, 1),
+(5, 6),
+(10, 3);
 
 -- --------------------------------------------------------
 
@@ -77,7 +102,7 @@ INSERT INTO `estudiantes` (`Id`, `nombre`, `edad`, `grado`) VALUES
 --
 
 CREATE TABLE `profesores` (
-  `Id` int(11) NOT NULL,
+  `id` int(11) NOT NULL,
   `nombre` varchar(50) DEFAULT NULL,
   `especialidad` varchar(50) DEFAULT NULL,
   `email` varchar(100) DEFAULT NULL
@@ -87,13 +112,33 @@ CREATE TABLE `profesores` (
 -- Volcado de datos para la tabla `profesores`
 --
 
-INSERT INTO `profesores` (`Id`, `nombre`, `especialidad`, `email`) VALUES
+INSERT INTO `profesores` (`id`, `nombre`, `especialidad`, `email`) VALUES
 (1, 'Rodrigo Gutierrez', 'civica', 'rodrigogutierrez@gmail.com'),
 (2, 'Carolina Saez', 'LAS', 'lacarosaez@gmail.com'),
 (3, 'Bruno Perez', 'COE', 'brunitop@hotmail.com'),
 (4, 'Ivana Fabrizio', 'desarrollo de aplicaciones', 'ivifabri@yahoo.com'),
 (5, 'Joel Rojas', 'matematica', 'joelitorojas@gmail.com'),
-(6, 'Maia Moreno', 'ingles', 'maiamoreno@yahoo.com');
+(6, 'Maia Moreno', 'ingles', 'maiamoreno@yahoo.com'),
+(8, 'cosme fulanito', 'contador', 'cosme@fulanito.com');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuarios`
+--
+
+CREATE TABLE `usuarios` (
+  `id` int(15) NOT NULL,
+  `username` char(50) NOT NULL,
+  `password` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `usuarios`
+--
+
+INSERT INTO `usuarios` (`id`, `username`, `password`) VALUES
+(7, 'admin', '$2a$10$frNfqTSlbLj9WwQABmTG5.BW/5nc620akJVs39KXm3h/jze8JNZt.');
 
 --
 -- Índices para tablas volcadas
@@ -103,19 +148,31 @@ INSERT INTO `profesores` (`Id`, `nombre`, `especialidad`, `email`) VALUES
 -- Indices de la tabla `cursos`
 --
 ALTER TABLE `cursos`
-  ADD PRIMARY KEY (`Id`);
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indices de la tabla `estudiantes`
 --
 ALTER TABLE `estudiantes`
-  ADD PRIMARY KEY (`Id`);
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `estudiantes_cursos`
+--
+ALTER TABLE `estudiantes_cursos`
+  ADD PRIMARY KEY (`id_curso`,`id_estudiante`);
 
 --
 -- Indices de la tabla `profesores`
 --
 ALTER TABLE `profesores`
-  ADD PRIMARY KEY (`Id`);
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- AUTO_INCREMENT de las tablas volcadas
@@ -125,19 +182,25 @@ ALTER TABLE `profesores`
 -- AUTO_INCREMENT de la tabla `cursos`
 --
 ALTER TABLE `cursos`
-  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT de la tabla `estudiantes`
 --
 ALTER TABLE `estudiantes`
-  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
 -- AUTO_INCREMENT de la tabla `profesores`
 --
 ALTER TABLE `profesores`
-  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  MODIFY `id` int(15) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
